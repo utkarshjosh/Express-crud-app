bd41a0dc693495b82bbca71cc99b650cd9e86c3b
utkarshjosh <utkarshjoshi7@gmail.com>
2021-11-25 17:42:14 +0530

    change for heroku

--- server.js
+++ server.js
@@ -32,4 +32,4 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 // load routers
 app.use('/', require('./server/routes/router'))
 
-app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+app.listen(process.env.PORT || 3000, ()=> { console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`)});
